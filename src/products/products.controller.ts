a4e14282a2059fb6f96b2b6df89868538e1c0d5e
Patricio Monpelat <patricio.monpelat@gmail.com>
2022-03-28 17:41:25 -0300

    update product

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,10 +1,10 @@
-import { Controller, Post, Body, Get, Param } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, Patch } from '@nestjs/common';
 import { ProductsService } from './products.service';
 
 @Controller('products')
 export class ProductsController {
     constructor(private readonly productsService: ProductsService) { }
-    
+
     @Post()
     addProduct(
         @Body('title') prodTitle: string,
@@ -12,17 +12,27 @@ export class ProductsController {
         @Body('price') prodPrice: number
     ) {
         const generatedId = this.productsService.insertProduct(prodTitle, prodDesc, prodPrice);
-        return {id: generatedId}
+        return { id: generatedId }
     }
 
     @Get()
-    getAllProducts(){
+    getAllProducts() {
         return this.productsService.getProducts();
     }
 
     @Get(':id')
-    getProduct(@Param('id') prodId: string ){
+    getProduct(@Param('id') prodId: string) {
         return this.productsService.getSingleProduct(prodId);
     }
+
+    @Patch(':id')
+    updateProduct(
+        @Param('id') prodId: string,
+        @Body('title') prodTitle: string,
+        @Body('description') prodDesc: string,
+        @Body('price') prodPrice: number
+        ) {
+            this.productsService.updateProduct(prodId, prodTitle, prodDesc, prodPrice);
+         }
 
 }
