acb82857f4291be69481c302f6a912b214dd7577
Patricio Monpelat <patricio.monpelat@gmail.com>
2022-03-28 17:21:33 -0300

    get all products & get single product

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param } from '@nestjs/common';
 import { ProductsService } from './products.service';
 
 @Controller('products')
@@ -13,6 +13,16 @@ export class ProductsController {
     ) {
         const generatedId = this.productsService.insertProduct(prodTitle, prodDesc, prodPrice);
         return {id: generatedId}
+    }
+
+    @Get()
+    getAllProducts(){
+        return this.productsService.getProducts();
+    }
+
+    @Get(':id')
+    getProduct(@Param('id') prodId: string ){
+        return this.productsService.getSingleProduct(prodId);
     }
 
 }
