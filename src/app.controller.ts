1b99ff3bcd8671a458b9cb207aa860d91720ad05
Patricio Monpelat <patricio.monpelat@gmail.com>
2022-06-28 16:53:50 -0300

    swagger added

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,13 @@
-import { Controller, Get, Header } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  @Header('Content-Type','text/html')
-  getHello(): string {
-    return this.appService.getHello();
-  }
+  // @Get()
+  // @Header('Content-Type','text/html')
+  // getHello(): string {
+  //   return this.appService.getHello();
+  // }
 }
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Controller, Post, Body, Get, Param, Patch, Delete } from '@nestjs/common';
 import { ProductsService } from './products.service';
 
